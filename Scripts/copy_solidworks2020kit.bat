a3d9d789e16c03a7a9bb5b58cc7251f2c89f1331
yyjtadmn <112881787+yyjtadmn@users.noreply.github.com>
2023-01-18 16:01:29 +0530

    Update copy_solidworks2020kit.bat

--- Scripts/copy_solidworks2020kit.bat
+++ Scripts/copy_solidworks2020kit.bat
@@ -1,4 +1,4 @@
-call robocopy  \\plm\pnnas\jtdev\Group_build\SW\SWV22.0\bld\sw2022\wntx64\Release\kit\solidworks "C:\Program Files\Siemens\JTTranslators\SolidworksJT_21.1" /MIR /XD License Help
+call robocopy  \\plm\pnnas\jtdev\Group_build\SW\SWV23.0\bld\sw2023\wntx64\Release\kit\solidworks "C:\Program Files\Siemens\JTTranslators\SolidworksJT_23.0.0" /MIR /XD License Help
 
 set/A errlev="%ERRORLEVEL% & 24"
 
