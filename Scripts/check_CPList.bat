b00bc84429081f6808ce881c4acc90ad92da7247
yyjtadmn <112881787+yyjtadmn@users.noreply.github.com>
2023-01-18 16:13:49 +0530

    Update check_CPList.bat

--- Scripts/check_CPList.bat
+++ Scripts/check_CPList.bat
@@ -1,8 +1,8 @@
 @echo off
 set productVersion=%1
-set logfile="P:\Data_Exchange\from_Roma\jenkins\CP_list.txt"
+set logfile="J:\Group_build\SW\CP_list.txt"
 call C:\\apps\\devop_tools\\perl.d\\bin\\perl.exe C:\\apps\\devop_tools\\bin\\dtcmd.pl cp qry -e %productVersion%>%logfile%
-rem find /v /c "" P:\Data_Exchange\from_Roma\jenkins\CP_list.txt
+rem find /v /c "" J:\Group_build\SW\CP_list.txt
 for /F "usebackq tokens=3 delims=:" %%L in (`find /v /c "" %logfile%`) do set lines=%%L
 SET /A totalCP=%lines%-1
 if %totalCP% lss 1 (echo 1) else (echo %totalCP%)
